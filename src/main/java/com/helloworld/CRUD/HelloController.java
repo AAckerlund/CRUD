728d276e61b48b80e32aeddf2e57dfd59881a59d
Alex <19612580+AAckerlund@users.noreply.github.com>
2023-11-23 12:47:48 -0700

    added other mappings

--- src/main/java/com/helloworld/CRUD/HelloController.java
+++ src/main/java/com/helloworld/CRUD/HelloController.java
@@ -1,13 +1,29 @@
 package com.helloworld.CRUD;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
+    @PostMapping("/hello")//Create
+    public String postHello()
+    {
+        return "This is the greatest CRUD app ever!!!";
+    }
 
-    @GetMapping("/hello")
-    public String hello()
+    @GetMapping("/hello")//Read
+    public String getHello()
+    {
+        return "This is the greatest CRUD app ever!!!";
+    }
+
+    @PutMapping("/hello")//Update
+    public String putHello()
+    {
+        return "This is the greatest CRUD app ever!!!";
+    }
+
+    @DeleteMapping("/hello")//Delete
+    public String deleteHello()
     {
         return "This is the greatest CRUD app ever!!!";
     }
